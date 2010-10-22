89ae4b0ecca51bc38230225d13a0d75793d72153
Craig McQueen <cmcqueen@aapl.com.au>
2010-10-22 13:57:37 +1100

    Python implements same algorithms and tests as C code.
    Implementing the tests in Python was a little messy due to the way the C code is structured: the C implementation's seed values aren't independent, unlike the Python implementation's.

--- python/simplerandom.py
+++ python/simplerandom.py
@@ -123,6 +123,60 @@ class RandomLFIB4(object):
     def __iter__(self):
         return self
 
+class RandomSWB(RandomLFIB4):
+    '''"Subtract-With-Borrow" random number generator
+    
+    This is a Fibonacci 2-lag generator with an extra "borrow" operation.
+    '''
+
+    def __init__(self, seed = None):
+        RandomLFIB4.__init__(self, seed)
+        self.borrow = 0
+
+    def next(self):
+        t = self.t
+
+        c = self.c
+        c = (c + 1) % 256
+        self.c = c
+
+        x = t[(c + 34) % 256]
+        y = (t[(c + 19) % 256] + self.borrow) & 0xFFFFFFFF
+        new_value = (x - y) & 0xFFFFFFFF
+
+        t[c] = new_value
+
+        self.borrow = 1 if (x < y) else 0
+
+        return new_value
+
+    def __iter__(self):
+        return self
+
+
+class RandomFib(object):
+    '''Classical Fibonacci sequence
+
+    x(n)=x(n-1)+x(n-2),but taken modulo 2^32. Its period is 3 * (2**31) if one
+    of its two seeds is odd and not 1 mod 8.
+    
+    It has little worth as a RNG by itself, but provides a simple and fast
+    component for use in combination generators.
+    '''
+
+    def __init__(self, seed_a, seed_b):
+        self.a = (seed_a & 0xFFFFFFFF)
+        self.b = (seed_b & 0xFFFFFFFF)
+
+    def next(self):
+        new_a = self.b
+        new_b = (self.a + self.b) & 0xFFFFFFFF
+        (self.a, self.b) = (new_a, new_b)
+        return new_a
+
+    def __iter__(self):
+        return self
+
 
 if __name__ == "__main__":
     if 0:
@@ -137,20 +191,6 @@ if __name__ == "__main__":
         shr3 = RandomSHR3(34221)
         for i in range(256):
             print next(shr3)
-    if 1:
-        random_kiss = RandomKISS(12345, 65435, 12345, 34221)
-        t = [ next(random_kiss) for i in range(256) ]
-        if 0:
-            for i in t:
-                print i
-        lfib4 = RandomLFIB4(t)
-        if 0:
-            for i in range(256):
-                print next(lfib4)
-        if 1:
-            for i in range(1000000):
-                k = next(lfib4)
-            print k-1064612766
     if 0:
         kiss = RandomKISS(12345, 65435, 12345, 34221)
         for i in range(256):
@@ -160,3 +200,50 @@ if __name__ == "__main__":
             print kiss.jsr
             print kiss_val
             print
+    if 1:
+        random_kiss = RandomKISS(12345, 65435, 12345, 34221)
+        t = [ next(random_kiss) for i in range(256) ]
+#        for i in t: print i
+
+        lfib4 = RandomLFIB4(t)
+        if 1:
+            for i in range(1000000):
+                k = next(lfib4)
+#                if i < 256: print k
+            print k - 1064612766
+
+        swb = RandomSWB(lfib4.t)
+        swb.c = lfib4.c
+        if 1:
+            for i in range(1000000):
+                k = next(swb)
+            print k - 627749721
+
+        if 1:
+            for i in range(1000000):
+                k = next(random_kiss)
+            print k - 1372460312
+
+        cong = RandomCong(random_kiss.cong)
+        if 1:
+            for i in range(1000000):
+                k = next(cong)
+            print k - 1529210297
+
+        shr3 = RandomSHR3(random_kiss.jsr)
+        if 1:
+            for i in range(1000000):
+                k = next(shr3)
+            print k - 2642725982
+
+        mwc = RandomMWC(random_kiss.z, random_kiss.w)
+        if 1:
+            for i in range(1000000):
+                k = next(mwc)
+            print k - 904977562
+
+        fib = RandomFib(9983651,95746118)
+        if 1:
+            for i in range(1000000):
+                k = next(fib)
+            print k - 3519793928
