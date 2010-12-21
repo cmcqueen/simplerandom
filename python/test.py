4e4d5f9b9506c7e46c8d6fc6d9aee09b656983ed
Craig McQueen <craig@mcqueen.id.au>
2010-12-21 16:26:14 +1100

    Make test.py simply import either simplerandom or simplerandomcython.

--- python/test.py
+++ python/test.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python2
 
-from simplerandom import *
-from simplerandom import _RandomFibIterator
-import simplerandomcython as src
+if 1:
+    from simplerandomcython import *
+    from simplerandomcython import _RandomFibIterator
+else:
+    from simplerandom import *
+    from simplerandom import _RandomFibIterator
 
 def marsaglia_test():
-    random_kiss = src.RandomKISSIterator(12345, 65435, 12345, 34221)
+    random_kiss = RandomKISSIterator(12345, 65435, 12345, 34221)
     t = [ next(random_kiss) for i in range(256) ]
 #        for i in t: print i
 
-    lfib4 = src.RandomLFIB4Iterator(t)
+    lfib4 = RandomLFIB4Iterator(t)
     if 1:
         for i in range(1000000):
             k = next(lfib4)
@@ -17,7 +20,7 @@ def marsaglia_test():
         print k - 1064612766
 
 #    swb = RandomSWBIterator(lfib4.getstate())
-    swb = src.RandomSWBIterator(lfib4.t)
+    swb = RandomSWBIterator(lfib4.t)
     swb.c = lfib4.c
     if 1:
         for i in range(1000000):
@@ -29,25 +32,25 @@ def marsaglia_test():
             k = next(random_kiss)
         print k - 1372460312
 
-    cong = src.RandomCongIterator(random_kiss.cong)
+    cong = RandomCongIterator(random_kiss.cong)
     if 1:
         for i in range(1000000):
             k = next(cong)
         print k - 1529210297
 
-    shr3 = src.RandomSHR3Iterator(random_kiss.shr3_j)
+    shr3 = RandomSHR3Iterator(random_kiss.shr3_j)
     if 1:
         for i in range(1000000):
             k = next(shr3)
         print k - 2642725982
 
-    mwc = src.RandomMWCIterator(random_kiss.mwc_z, random_kiss.mwc_w)
+    mwc = RandomMWCIterator(random_kiss.mwc_z, random_kiss.mwc_w)
     if 1:
         for i in range(1000000):
             k = next(mwc)
         print k - 904977562
 
-    fib = src._RandomFibIterator(9983651,95746118)
+    fib = _RandomFibIterator(9983651,95746118)
     if 1:
         for i in range(1000000):
             k = next(fib)
@@ -60,20 +63,20 @@ if __name__ == "__main__":
         for i in range(256):
             print next(mwc)
     if 0:
-        cong = src.RandomCongIterator(12345)
+        cong = RandomCongIterator(12345)
         for i in range(256):
             print next(cong)
     if 0:
-        shr3 = src.RandomSHR3Iterator(34221)
+        shr3 = RandomSHR3Iterator(34221)
         for i in range(256):
             print next(shr3)
     if 0:
-        shr3 = src.RandomSHR3Iterator(34221)
+        shr3 = RandomSHR3Iterator(34221)
         for i in range(2000000):
             k = next(shr3)
         print k
     if 0:
-        kiss = src.RandomKISSIterator(12345, 65435, 12345, 34221)
+        kiss = RandomKISSIterator(12345, 65435, 12345, 34221)
         for i in range(256):
             kiss_val = next(kiss)
             print kiss.mwc
