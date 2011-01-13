1dfafbd5bb50db9a49a2549c6fd2ff3c0868b768
Craig McQueen <craig@mcqueen.id.au>
2011-01-13 12:39:10 +1100

    Add Python 3.2 category to setup.py.

--- python/setup.py
+++ python/setup.py
@@ -62,6 +62,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
         'Programming Language :: Cython',
         'Topic :: Scientific/Engineering :: Mathematics',
     ],
