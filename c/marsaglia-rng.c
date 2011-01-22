32a3d02312c5f90ead39548ad173953a1b26bbf9
Craig McQueen <craig@mcqueen.id.au>
2011-01-23 09:59:54 +1100

    Reorder variables. Change those that don't need to be static.

--- c/marsaglia-rng.c
+++ c/marsaglia-rng.c
@@ -20,15 +20,19 @@
 #define VNI (((long) KISS)*4.656613e-10)
 
 /* Global static variables: */
-static uint32_t mwc_upper = 362436069, mwc_lower = 521288629, shr3 = 362436000, cong = 380116160;
+/* Use random seeds to reset mwc_upper,mwc_lower,shr3,cong,fib_a,fib_b, and the table t[256]*/
+static uint32_t mwc_upper = 362436069, mwc_lower = 521288629;
+static uint32_t shr3 = 362436000;
+static uint32_t cong = 380116160;
 static uint32_t fib_a = 224466889, fib_b = 7584631;
 static uint32_t t[256];
 
+static uint32_t swb_bro = 0;
+static uint8_t c = 0;
+
 static uint64_t mwc64 = MWC64_SEED(7654321, 521288629);
 
-/* Use random seeds to reset mwc_upper,mwc_lower,shr3,cong,a,b, and the table t[256]*/
-static uint32_t swb_x = 0, swb_y = 0, swb_bro = 0;
-static uint8_t c = 0;
+uint32_t swb_x = 0, swb_y = 0;
 
 /* Example procedure to set the table, using KISS: */
 void settable(uint32_t i1, uint32_t i2, uint32_t i3, uint32_t i4, uint32_t i5, uint32_t i6)
