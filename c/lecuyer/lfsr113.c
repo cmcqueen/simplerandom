b0840e5326fbea9baad203cffe00c8eb473512c0
Craig McQueen <craig@mcqueen.id.au>
2011-02-16 06:13:22 +1100

    Add an integer implementation. Add header file.
    Same as double implementation, except no final conversion to double.

--- c/lecuyer/lfsr113.c
+++ c/lecuyer/lfsr113.c
@@ -17,7 +17,7 @@
 static unsigned int z1 = SEED, z2 = SEED, z3 = SEED, z4 = SEED;
 
 
-double lfsr113 (void)
+double lfsr113_double (void)
 {
    unsigned int b;
    b  = ((z1 << 6) ^ z1) >> 13;
@@ -29,4 +29,18 @@ double lfsr113 (void)
    b  = ((z4 << 3) ^ z4) >> 12;
    z4 = ((z4 & 4294967168UL) << 13) ^ b;
    return (z1 ^ z2 ^ z3 ^ z4) * 2.3283064365386963e-10;
+}
+
+unsigned int lfsr113 (void)
+{
+   unsigned int b;
+   b  = ((z1 << 6) ^ z1) >> 13;
+   z1 = ((z1 & 4294967294UL) << 18) ^ b;
+   b  = ((z2 << 2) ^ z2) >> 27; 
+   z2 = ((z2 & 4294967288UL) << 2) ^ b;
+   b  = ((z3 << 13) ^ z3) >> 21;
+   z3 = ((z3 & 4294967280UL) << 7) ^ b;
+   b  = ((z4 << 3) ^ z4) >> 12;
+   z4 = ((z4 & 4294967168UL) << 13) ^ b;
+   return (z1 ^ z2 ^ z3 ^ z4);
 }
---
+++ c/lecuyer/lfsr113.h
@@ -0,0 +1,11 @@
+/* 
+   32-bits Random number generator U[0,1): lfsr113
+   Author: Pierre L'Ecuyer,
+   Source: http://www.iro.umontreal.ca/~lecuyer/myftp/papers/tausme2.ps 
+   ---------------------------------------------------------
+*/
+
+double lfsr113_double (void);
+unsigned int lfsr113 (void);
+
+
