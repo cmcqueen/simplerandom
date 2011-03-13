b1774b0ed71bc6f089d1c9277c8d430fa90af1d4
Craig McQueen <craig@mcqueen.id.au>
2011-03-14 00:12:39 +1100

    Add comments with results of small-crush tests.

--- support/mwc_tests/mwc_tests.c
+++ support/mwc_tests/mwc_tests.c
@@ -2,6 +2,25 @@
 #include "bbattery.h"
 
 
+/*
+========= Summary results of SmallCrush =========
+
+ Version:          TestU01 1.2.3
+ Generator:        mwc1
+ Number of statistics:  15
+ Total CPU time:   00:00:28.26
+ The following tests gave p-values outside [0.001, 0.9990]:
+ (eps  means a value < 1.0e-300):
+ (eps1 means a value < 1.0e-15):
+
+       Test                          p-value
+ ----------------------------------------------
+  2  Collision                      1 - eps1
+  3  Gap                             1.2e-4
+  6  MaxOft                           eps  
+ ----------------------------------------------
+ All other tests were passed
+*/
 unsigned int mwc1(void)
 {
     static unsigned int mwc_upper = 362436069u;
@@ -13,6 +32,23 @@ unsigned int mwc1(void)
     return (mwc_upper << 16u) + (mwc_lower & 0xFFFF);
 }
 
+/*
+========= Summary results of SmallCrush =========
+
+ Version:          TestU01 1.2.3
+ Generator:        mwc2
+ Number of statistics:  15
+ Total CPU time:   00:00:27.80
+ The following tests gave p-values outside [0.001, 0.9990]:
+ (eps  means a value < 1.0e-300):
+ (eps1 means a value < 1.0e-15):
+
+       Test                          p-value
+ ----------------------------------------------
+  3  Gap                             1.2e-4
+ ----------------------------------------------
+ All other tests were passed
+*/
 unsigned int mwc2(void)
 {
     static unsigned int mwc_upper = 362436069u;
@@ -24,6 +60,16 @@ unsigned int mwc2(void)
     return (mwc_upper << 16u) + mwc_lower;
 }
 
+/*
+========= Summary results of SmallCrush =========
+
+ Version:          TestU01 1.2.3
+ Generator:        mwc3
+ Number of statistics:  15
+ Total CPU time:   00:00:28.14
+
+ All tests were passed
+*/
 unsigned int mwc3(void)
 {
     static unsigned int mwc_upper = 362436069u;
