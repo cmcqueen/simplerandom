6eedf68c93d306b208254f82c1220ce441ea1c28
Craig McQueen <craig.mcqueen@innerrange.com>
2023-10-16 11:55:41 +1100

    Restructure and change packaging, according to current advice
    https://packaging.python.org/en/latest/overview/
    
    Advice on binary extensions is unclear.
    https://packaging.python.org/en/latest/guides/packaging-binary-extensions/
    
    Advice on Cython extensions is unclear.

--- python/setup.py
+++ python/setup.py
@@ -13,8 +13,6 @@ import sys
 from setuptools import setup
 from setuptools import Extension
 
-import _version
-
 if USE_CYTHON:
     try:
         from Cython.Distutils import build_ext
@@ -43,36 +41,11 @@ else:
     ]
 
 setup(
-    name='simplerandom',
-    version=_version.__version__,
-    description='Simple random number generators',
-    author='Craig McQueen',
-    author_email='python@craig.mcqueen.id.au',
-    url='http://github.com/cmcqueen/simplerandom',
     packages=[ 'simplerandom', 'simplerandom.iterators', 'simplerandom.random', 'simplerandom._bitcolumnmatrix', 'simplerandom._version', ],
     package_dir={
-        'simplerandom' : 'python3/simplerandom',
-        'simplerandom._version' : '_version',
+        'simplerandom' : 'src/simplerandom',
     },
     cmdclass = cmdclass,
     ext_modules=ext_modules,
-
-    long_description=open('README.rst').read(),
-
-    license="MIT",
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Cython',
-        'Topic :: Scientific/Engineering :: Mathematics',
-    ],
-    keywords='simple random pseudorandom RNG PRNG',
 )
 
